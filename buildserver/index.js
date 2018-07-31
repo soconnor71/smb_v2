b47aa5a1b3961fc624f765932859ca4b07032e60
pellsson <pellelindblad@gmail.com>
2018-07-31 15:37:20 +0200

    fix bs

---
+++ buildserver/index.js
@@ -0,0 +1,152 @@
+const express = require('express')
+const fs = require('fs')
+const { execFile } = require('child_process');
+const path = require('path')
+
+const app = express()
+const port = 8888
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+function write_splits(res, worlds) {
+	var world_rules = ''
+	var pup_bytes = '.db '
+
+	for(let i = 0; i < worlds.length; ++i)
+	{
+		let levels = worlds[i]
+		if(!levels || 4 != levels.length)
+		{
+			res.status(500).send('Invalid number of levels for world ' + (i + 1))
+			return
+		}
+
+		world_rules += '.dw '
+		let enc_lives = 0
+
+		for(let x = 0; x < levels.length; ++x)
+		{
+			let v = levels[x].split(':')
+			if(2 != v.length)
+			{
+				res.status(500).send('Invalid level value')
+				return
+			}
+
+			let rules = Number(v[0])
+			let pups = Number(v[1])
+
+			if(isNaN(rules) || isNaN(pups))
+			{
+				res.status(500).send('Not an integer')
+				return
+			}
+
+			if(rules > 9999 || pups > 2)
+			{
+				res.status(500).send('Too high rule, or too many pups')
+				return
+			}
+
+			enc_lives = enc_lives | (pups << (x * 2))
+
+			world_rules += '$' + rules
+
+			if((x + 1) != levels.length)
+			{
+				world_rules += ', '
+			}
+		}
+		
+		pup_bytes += '$' + enc_lives.toString(16)
+		if((i + 1) != worlds.length)
+		{
+			pup_bytes += ', '
+		}
+
+		world_rules += '\n'
+	}
+
+	// console.log(pup_bytes)
+	// console.log(world_rules)
+
+	fs.readFile('../practice.asm', 'utf8', (err, data) => {
+		if(err) {
+			console.log('Reading:' + err)
+			res.status(500).send('Something broke horribly')
+			return
+		}
+
+		data = data.replace(/;<BUILD_PATCH_LEVELS>[\s\S]+;<\/BUILD_PATCH_LEVELS>/m, world_rules)
+		data = data.replace(/;<BUILD_PATCH_PUPS>[\s\S]+;<\/BUILD_PATCH_PUPS>/m, pup_bytes)
+
+		var file_base = 'builds/practice-' + (new Date().getTime())
+
+		fs.writeFile(file_base + '.asm', data, (err) => {
+			if(err) {
+				console.log('Writing: ' + err)
+				res.status(500).send('Something broke horribly')
+				return
+			}
+
+			execFile('bash', [ 'build.sh', file_base + '.asm' ], { cwd: '..' }, (err, stdout, stderr) => {
+				if(err) {
+					console.log('bash: ', err)
+					res.status(500).send('Build exploded somehow <.<')
+					return
+				}
+				
+				res.setHeader('Content-Disposition', 'attachment; filename=smbpractice.nes');
+				res.setHeader('Content-Transfer-Encoding', 'binary');
+				res.setHeader('Content-Type', 'application/octet-stream');
+
+				res.sendFile(path.join(__dirname, '..', file_base + '.nes'))
+			})
+		})
+	})
+}
+
+app.get('/', function(req, res) {
+	//
+	// To get an idea if anyone is using it...
+	//
+	const ip = req.connection.remoteAddress || '<no idea>'
+	console.log('Access root: ' + ip)
+
+	res.render('index')
+})
+
+app.get('/practice', function(req, res) {
+	//
+	// To get an idea if anyone is using it...
+	//
+	const ip = req.connection.remoteAddress || '<no idea>'
+	console.log('Access /practice: ' + ip)
+
+	res.render('practice')
+})
+
+app.get('/build', function(req, res) {
+	console.log('Got build request!')
+	write_splits(res,
+		[
+			req.query.w1,
+			req.query.w2,
+			req.query.w3,
+			req.query.w4,
+			req.query.w5,
+			req.query.w6,
+			req.query.w7,
+			req.query.w8
+		]
+	)
+})
+
+app.listen(port, function() {
+	console.log('SMB Practice ROM - Buildserver on port: ' + port)
+})
+
+
+
---
+++ buildserver/public/splits.js
@@ -0,0 +1,278 @@
+var split_rule = true
+
+function wsplit_get_param(key, lines, index, dispatch, is_opt)
+{
+	if(lines[index].startsWith(key + '='))
+	{
+		dispatch(lines[index].substring(key.length + 1))
+		return index + 1;
+	}
+
+	if(is_opt)
+	{
+		return index;
+	}
+
+	throw "Invalid WSplit - Missing key: " + key
+}
+
+function populate_split_row(r, arr)
+{
+	for(let i = 0; i < arr.length; ++i)
+	{
+		let c = r.insertCell(-1)
+		c.innerHTML = arr[i]
+	}
+}
+
+function set_download(str)
+{
+	document.getElementById('download').innerHTML = str
+}
+
+function get_rule(index)
+{
+	return document.getElementById('rule_' + index).value
+}
+
+function collects_life(index)
+{
+	let cl = document.getElementById('collect_' + index)
+	if(!cl)
+	{
+		return false
+	}
+	return cl.checked
+}
+
+function get_splits_body()
+{
+	return document.getElementById('splits').getElementsByTagName('tbody')[0]
+}
+
+function rebuild_download()
+{
+	var pups_collected = 0
+	var splits = get_splits_body().rows.length
+	var worlds;
+
+	if(8 == splits)
+	{
+		worlds =
+		[
+			[  0,  1, -1, -1 ],
+			[ -1, -1, -1, -1 ],
+			[ -1, -1, -1, -1 ],
+			[  2,  3, -1, -1 ],
+			[ -1, -1, -1, -1 ],
+			[ -1, -1, -1, -1 ],
+			[ -1, -1, -1, -1 ],
+			[  4,  5,  6,  7 ]
+		]
+	}
+	else if(32 == splits)
+	{
+		var total = 0
+		worlds = []
+
+		for(var w = 0; w < 8; ++w)
+		{
+			v = []
+			for(var i = 0; i < 4; ++i)
+			{
+				v.push(total++)
+			}
+			worlds.push(v)
+		}
+	}
+	else
+	{
+		set_download('I dont understand anything. I broke really bad <.< ' + splits.length)
+		return
+	}
+
+	var num_pups = 0
+	var prev = 0
+
+	for(var i = 0; i < worlds.length; ++i)
+	{
+		for(var x = 0; x < worlds[i].length; ++x)
+		{
+			var rule_index = worlds[i][x]
+
+			if(-1 != rule_index)
+			{
+				let rule = Number(get_rule(rule_index))
+
+				if(rule > 9999)
+				{
+					set_download('Max rule is 9999')
+					return
+				}
+
+				if(prev > rule)
+				{
+					set_download('Earlier split has higher framerule than the one after')
+					return
+				}
+
+				if(collects_life(rule_index))
+				{
+					if(++num_pups > 2)
+					{
+						set_download('You can collect 2 power-ups at most')
+						return
+					}
+				}
+
+				prev = rule
+				worlds[i][x] = rule + ':' + num_pups
+			}
+			else
+			{
+				worlds[i][x] = '0:0'
+			}
+		}
+	}
+
+	var dl = '/build?'
+
+	for(var i = 0; i < worlds.length; ++i)
+	{
+		for(var x = 0; x < worlds[i].length; ++x)
+		{
+			dl += 'w' + (i + 1) + '=' + worlds[i][x] + '&'
+		}
+	}
+	set_download('<a href="' + dl +  '">Download Ready!</a>')
+}
+
+function rule_entry(name, total, duration, rule)
+{
+	return { name, total, duration, rule }
+}
+
+function build_table(rules)
+{
+	tbl = get_splits_body()
+	tbl.innerHTML = ''
+
+	for(let i = 0; i < rules.length; ++i)
+	{
+		let v = []
+
+		v.push('<b>' + rules[i].name + '</b>')
+		v.push(rules[i].total)
+		v.push(rules[i].duration)
+		v.push('<input type="text" id="rule_' + i + '" size="4" value="' + rules[i].rule + '" oninput="rebuild_download()" />')
+		if(0 != i)
+		{
+			v.push('<input type="checkbox" id="collect_' + i + '" onclick="rebuild_download()" />')
+		}
+		else
+		{
+			v.push('');
+		}
+
+		populate_split_row(tbl.insertRow(-1), v)
+	}
+
+	rebuild_download()
+}
+
+function round_time(v)
+{
+	return Math.round(v * 100) / 100
+}
+
+function update_splits()
+{
+	try
+	{
+		var index = 0;
+		let lines = document.getElementById('wsplit').value.trim().split('\n')
+
+		index = wsplit_get_param('Title', lines, index, function(v) {
+			document.getElementById('title_id').innerHTML = v
+		})
+		index = wsplit_get_param('Goal', lines, index, function(v) {}, true)
+		index = wsplit_get_param('Attempts', lines, index, function(v) {
+			document.getElementById('attempts_id').innerHTML = v
+		})
+		index = wsplit_get_param('Offset', lines, index, function(v) {})
+		index = wsplit_get_param('Size', lines, index, function(v) {})
+
+		let splits = []
+		let num_splits = lines.length - index - 1;
+
+		if(8 != num_splits && 32 != num_splits)
+		{
+			throw 'Expected 8 or 32 splits, found: ' + num_splits
+		}
+
+		let rules = []
+		let start_rule = Number(document.getElementById('start_rule').value);
+		let start_time = round_time(start_rule * (21/60))
+		//
+		// First possible rule to press start is 2, then 7 to get into 1-1
+		//
+		let rule_offset = 2 + 7
+
+		rules.push(rule_entry('Enter 1-1',  start_time,  start_time, start_rule + rule_offset));
+
+		for(let i = 0; i < (num_splits - 1); ++i)
+		{
+			let v = lines[index++].split(',')
+			if(4 != v.length)
+			{
+				throw 'Failed to parse split-entry, expected 4 tokens, got ' + v.length
+			}
+
+			v.splice(1, 1)
+			let rule = (v[1] * 60) / 21
+
+			if(split_rule)
+			{
+				if(8 == num_splits && (2 != i && 4 != i))
+				{
+					//
+					// In warpless, all splits are split at Intermediate screen.
+					// In any%, world 4-1 and world 8-1 are split in accordance with the Practice ROM.
+					//
+					rule_offset += 7
+				}
+			}
+
+			rule += start_rule
+			rule += rule_offset
+
+			rules.push(rule_entry(v[0], round_time(v[1]), round_time(v[2]), Math.round(rule)));
+		}
+
+		index++;
+		wsplit_get_param('Icons', lines, index, function(v) {})
+
+		build_table(rules)
+	}
+	catch(err)
+	{
+		console.log(err)
+		set_download(' ERROR - ' + err)
+	}
+}
+
+function splits_pasted()
+{
+	$("#info").fadeIn(500)
+	$("#config").fadeIn(500)
+	update_splits()
+}
+
+function change_split_mode(on_rule)
+{
+	split_rule = on_rule
+	update_splits()
+}
+
+
+
