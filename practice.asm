f861dd68693664f9c77b793c648c031d19148008
pellsson <pellelindblad@gmail.com>
2018-08-08 04:05:28 +0200

    kill second player stuff... man...

--- practice.asm
+++ practice.asm
@@ -4343,9 +4343,6 @@ GameMode:
 ;-------------------------------------------------------------------------------------
 
 GameCoreRoutine:
-      ldx CurrentPlayer          ;get which player is on the screen
-      lda SavedJoypadBits,x      ;use appropriate player's controller bits
-      sta SavedJoypadBits        ;as the master controller bits
       jsr GameRoutines           ;execute one of many possible subs
       lda OperMode_Task          ;check major task of operating mode
       cmp #$03                   ;if we are supposed to be here,
