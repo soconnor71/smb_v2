2363ff09aa89a0550d9f59421f433cefac56bd53
pellsson <pellelindblad@gmail.com>
2018-09-29 15:29:02 +0200

    fixes

--- ll-analyze/fixasm.py
+++ ll-analyze/fixasm.py
@@ -1,31 +1,47 @@
 import sys
 import re
 
+def fix_argument(a, comment):
+	c = comment.split()
+	if len(c) >= 2:
+		if 'RENAME' == c[0] or 'RELOC' == c[0]:
+			return ' '.join(c[1:])
+	return a
+
 def fix_instruction(inst, comment):
 	if 0 == len(inst):
 		return ''
-	tokens = [ it.strip() for it in inst.split() ]
+	if 'REMOVE' == comment:
+		return '\t\t; ' + inst
+
+	tokens = [ it.strip() for it in inst.split(None, 1) ]
 	if '.BYTE' == tokens[0]:
-		return '\t\t' + '.db ' + tokens[1]
+		return '\t\t' + '.db ' + fix_argument(tokens[1], comment)
 	elif '.WORD' == tokens[0]:
-		return '\t\t' + '.dw ' + tokens[1]
+		m = re.match(r'\$[0-9a-fA-F]{4}.*', fix_argument(tokens[1], comment))
+		if m:
+			if '$ffff' != tokens[1].lower():
+				raise 'nope'
+		s = ''
+		if 'NonMaskableInterrupt' == tokens[1]:
+			s = '\t.seekoff $FFFA $EA\n'
+		return s + '\t\t' + '.dw ' + tokens[1]
 
 	if len(tokens) > 2:
 		print(inst)
 		raise 'dont understand'	
 	mnem = tokens[0].lower()
 	s = '\t\t' + mnem
-	if mnem in [ 'lsr' ]:
+	if mnem in [ 'lsr', 'asl', 'rol', 'ror' ]:
 		tokens.pop()
 	if 2 == len(tokens):
-		hack = False
-		c = comment.split()
-		if len(c) >= 2:
-			if 'RENAME' == c[0]:
-				tokens[1] = ' '.join(c[1:])
+		tokens[1] = fix_argument(tokens[1], comment)
 		m = re.match(r'\$[0-9a-fA-F]{4}.*', tokens[1])
 		if m:
 			print(tokens[1])
+			raise 'nope'
+		if tokens[1][-2:] == ',X' or tokens[1][-2:] == ',Y':
+			tokens[1] = tokens[1][0:-2] + tokens[1][-2:].lower()
 		s += ' ' + tokens[1]
 	return s
 
@@ -41,13 +57,24 @@ if beg == len(lines):
 	print('Nah')
 	sys.exit(-1)
 
-fixed.append('\t.org $c000')
-
+fixed.append('\t.org $8000')
+fixed.append('\t.vars vars.inc')
+fixed.append('\tFDS_Delay132:') # kill ones it builds.
+fixed.append('\tFDS_LoadFiles:') # kill ones it builds.
 for i in range(beg + 1, len(lines)):
 	line = lines[i].decode('utf-8', errors = 'ignore')
 	parts = [ it.strip() for it in line.split(';', 1) ]
 	line = parts[0]
 	comment = '' if len(parts) < 2 else parts[1]
+	for j in range(i + 1, len(lines)):
+		next_line = lines[j].decode('utf-8', errors = 'ignore').strip()
+
+		if len(next_line) and '; End' == next_line[0:5]:
+			break
+		if len(next_line) and ';' == next_line[0]:
+			comment = next_line[1:].strip()
+		else:
+			break
 	if 0 == len(line):
 		continue
 	if '*' == line[0]:
